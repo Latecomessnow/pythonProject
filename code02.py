a7f073f85f003d3b3117a6da15f4a86c00084783
古慧 <2874233502@qq.com>
2023-08-09 23:31:21 +0800

    practice

---
+++ code02.py
@@ -0,0 +1,4 @@
+avg = (67.5 + 90 + 76.8 + 34.8) / 4
+total = (67.2 - avg) ** 2 + (90 - avg) ** 2 + (76.8 - avg) ** 2 + (34.8 - avg) ** 2
+result = total / 4
+print(result)
---
+++ code03.py
@@ -0,0 +1,28 @@
+a = 10
+print(a)
+a = 20.0
+print(a)
+print(type(a))
+str = "hello world"
+print(type(str))
+str2 = "My name is 'DaMing'"
+print(type(str2))
+str3 = '''"hello" "Python"'''
+print(str3)
+print(type(str3))
+
+
+print(len(str))
+print(len(str3))
+print(len(str + " " + str2))
+
+b = True
+c = False
+print(type(b))
+print(type(c))
+
+# 注释
+
+'''
+这是一个注释
+'''
