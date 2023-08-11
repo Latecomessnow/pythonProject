707ec91cff63ceb6c09b93983f9a0c063107625b
古慧 <2874233502@qq.com>
2023-08-11 20:38:36 +0800

    Python

---
+++ code07.py
@@ -0,0 +1,11 @@
+# year = int(input("请输入年份: "))
+# if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+#     print("闰年")
+# else:
+#     # pass
+#     print("平年")
+
+num = 1
+while num <= 10:
+    print(num)
+    num += 1
---
+++ code08.py
@@ -0,0 +1,13 @@
+# for i in range(1, 11):
+#     print(i)
+
+for i in range(2, 11, 2):
+    print(i)
+
+for i in range(9, 0, -2):
+    print(i)
+
+theSum = 0
+for i in range(1, 101):
+    theSum += i
+print(f"sum = {theSum}")
